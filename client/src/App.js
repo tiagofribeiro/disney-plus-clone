1853043f8545037831ed531f4b0147792d6759e3
tiagofribeiro <tg.frnds@outlook.com>
2023-09-05 20:50:22 -0300

    Router created
    Login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
-      <h2>Placeholder</h2>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
